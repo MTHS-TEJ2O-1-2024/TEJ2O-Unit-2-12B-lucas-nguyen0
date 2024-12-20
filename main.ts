d26e899f6bcb2170bd2c28b44ec1eb09d6db1ab5
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-12-20 11:06:03 -0500

    fixed comment

--- main.ts
+++ main.ts
@@ -1,8 +1,57 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Nov 2024
+ * This program senses distance, and displays a light.
 */
 
-basic.showString('Hello, World!')
+//variables
+let neopixelStrip: neopixel.Strip = null
+let objectDistance: number = 0
+
+//setup & cleanup
+basic.showIcon(IconNames.Happy)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+//find distance from sonar
+input.onButtonPressed(Button.A, function () {
+    objectDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    basic.clearScreen()
+    basic.showNumber(objectDistance), basic.showString('cm')
+
+    // if distance was < 10
+    if (objectDistance < 10) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
+    } else {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Sad)
+    }
+
+    // cleanup
+    pause(1000)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
